a8daf3f501e6d92a6625a1a0d72d3314d55c764c
imcoolthanks <wongq9999@outlook.com>
2023-02-05 06:39:44 +0000

    Plotting function according to company

--- Flask/app.py
+++ Flask/app.py
@@ -2,7 +2,9 @@
 
 from flask import Flask, render_template, Response, request, redirect, url_for
 import sqlite3 as sql
-import json
+import sys        
+
+import plot
 
 UPLOAD_FOLDER = "../Flask/logFiles"
 ALLOWED_EXTENSIONS = set(['.csv', '.txt'])
@@ -24,7 +26,8 @@ def benefits():
 
 @app.route('/dashboard/', methods=["GET"])
 def dashboard():
-    return render_template("dashboard.html", company=request.args.get('name'))
+    name = request.args.get('name')
+    return render_template("dashboard.html", company=name, graph_url='Assets/graphs/' + name + ".png")
 
 @app.route("/login/", methods = ['POST', 'GET'])
 def login():                
@@ -36,7 +39,7 @@ def login():
         
         if success:
             #Does stuff to load the website
-
+            graph(company)
             return redirect(url_for('.dashboard', name=company))
         else:
             return render_template('login.html', error="Incorrect email or password.")
@@ -64,6 +67,11 @@ def _login(email, password):
         print("Wrong Password")
         return False, ""
 
+def graph(company):
+    print("Graphing")
+    get_data(company)
+    plot(company)
+
 @app.route('/setting/')
 def setting():
     return render_template("setting.html")
@@ -78,3 +86,96 @@ def csv_download():
         mimetype="text/csv",
         headers={"Content-disposition":
                  "attachment; filename=myplot.csv"})
+
+#PLOTTING FUNCTINONS
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+import csv
+import datetime
+
+import sqlite3 as sql
+
+# Line graphs
+# - Raw materials used in a year
+# - CO2 emissions
+# - Percentage of foreign imports of materials
+# - Percentage of reusable material
+
+def get_data(company):
+    raw_materials_data = []
+    co2_data = []
+    percentage_foreign = []
+    percentage_reusable = []
+        
+    conn = sql.connect("companies.db")
+    cur = conn.cursor()
+
+    cur.execute("select * from " + company)
+    
+    rows = list(cur.fetchall())
+
+    conn.close()
+
+    for r in rows:
+        raw_materials_data.append(r[0])
+        co2_data.append(r[1])
+        percentage_foreign.append(r[2])
+        percentage_reusable.append(r[3])
+
+    return raw_materials_data, co2_data, percentage_foreign, percentage_reusable
+
+def plot(company):
+    years = []
+
+    raw_materials_data, co2_data, percentage_foreign, percentage_reusable = get_data(company)
+
+    # get all the years
+    current_year = datetime.date.today().year
+    for i in range(len(raw_materials_data)):
+        years.append(current_year - i)
+    years.reverse
+
+    #Plotting data
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(8,8))
+
+    fig.suptitle('Sustainablity of ' + company)
+
+    ax1.plot(years, raw_materials_data)
+    ax1.scatter(years, raw_materials_data, s = 10, color='black')
+    ax1.title.set_text('Raw materials used in a year')
+    plt.xticks(np.arange(min(years), max(years)+1, 1.0))
+    ax1.set_ylabel("Tonnes")
+    for index in range(len(years)):
+        ax1.text(years[index], raw_materials_data[index], raw_materials_data[index], size=6)
+
+
+    ax2.plot(years, co2_data)
+    ax2.scatter(years, co2_data, s = 10, color='black')
+    ax2.title.set_text('CO2 emissions')
+    plt.xticks(np.arange(min(years), max(years)+1, 10.0))
+    ax2.set_ylabel("Mass /g")
+    for index in range(len(years)):
+        ax2.text(years[index], co2_data[index], co2_data[index], size=6)
+
+    ax3.plot(years, percentage_foreign)
+    ax3.scatter(years, percentage_foreign, s = 10, color='black')
+    ax3.title.set_text('Percentage of foreign imports of materials')
+    plt.xticks(np.arange(min(years), max(years)+1, 1.0))
+    ax3.set_ylabel("Mass /g")
+    for index in range(len(years)):
+        ax3.text(years[index], percentage_foreign[index], percentage_foreign[index], size=6)
+
+    ax4.plot(years, percentage_reusable)
+    ax4.scatter(years, percentage_reusable, s = 10, color='black')
+    ax4.title.set_text('Percentage of reusable material')
+    plt.xticks(np.arange(min(years), max(years)+1, 1.0))
+    ax4.set_ylabel("Mass /g")
+    for index in range(len(years)):
+        ax4.text(years[index], percentage_reusable[index], percentage_reusable[index], size=6)
+
+    fig.tight_layout()
+
+    #Save Graph
+    save_url = 'Assets/graphs/'+company+'.png'
+    plt.savefig('Flask/static/'+ save_url)
